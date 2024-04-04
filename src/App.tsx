6a3fc5b5f9269c4ba80b346bc5ddf77441627658
Willian Peres <willianperesv@hotmail.com>
2024-04-04 00:33:24 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyled } from './styles/global'
+import { Router } from './Router';
+
+
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+       <Router/>
+      </BrowserRouter>
+      
+
+    <GlobalStyled/>
+    </ThemeProvider>
+  )
+}
+
