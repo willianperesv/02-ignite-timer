b58749df637371e4fdf5b5ca309329aeb60512fc
Willian Peres <willianperesv@hotmail.com>
2024-04-04 21:09:59 -0300

    Contadores

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { Play } from "phosphor-react";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+
 import {
     CountdownContainer,
     FormContainer,
@@ -8,45 +12,134 @@ import {
     StartCountdownButton,
     TaskInput
 } from "./styles";
+import { useState } from "react";
+
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number().min(5, 'O ciclo precisa ser de no minimo 5 minutos ').max(60, 'O ciclo precisa ser de no máximo 60 minutos '),
+})
+
+// interface NewCycleFormData {
+//     task: string,
+//     minutesAmount: number
+// }
+
+
+//inferindo tipagem atráves do zod
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 
 export function Home() {
+    
+    interface Cycle {
+        id: string,
+        task:string,
+        minutesAmount:number,
+    }
+
+    const [cycles, setCycles] = useState<Cycle[]>([]);
+    const [activeCycleId, setActiveCycledId] = useState<string | null>(null);
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0
+        }
+    });
+
+
+
+    function handleCreateNewCycle(data: NewCycleFormData) {
+        const id = String(new Date().getTime());
+
+        const newCycle: Cycle ={
+            id,
+             task: data.task,
+             minutesAmount: data.minutesAmount
+        }
+
+        setCycles((state) => [...state, newCycle])
+        setActiveCycledId(id)
+        reset();
+    }
+
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+    
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmout = currentSeconds % 60;
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmout).padStart(2, '0')
+
+    const task = watch('task')
+    const isSubmitDisabled = !task;
+
+
+    // Modo Controlled
+    //const [task, setTask] = useState('');
+
+    // function resetForm(){
+    //     setTask('');
+    // }
+
+
+    // A função register retorna varios eventos nativos de javascript
+
+    /* function register(name:string, ){
+        return{
+            onChange: () => void,
+            onBlur: () => void,
+            onFocus: () =>void
+        }
+    }*/
+
     return (
         <HomeContainer>
-            <form action="">
-                <FormContainer>              
-                        <label htmlFor="task">Vou trabalhar em</label>
-                        <TaskInput
-                            id="task"
-                            list="task-suggestions"
-                            placeholder="Dê um nome para seu projeto" />
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput
+                        id="task"
+                        list="task-suggestions"
+                        placeholder="Dê um nome para seu projeto"
+                        {...register('task')}
+                    />
 
-                            <datalist id="task-suggestions">
-                                <option value="Projeto 1"/>
-                                <option value="Projeto 2"/>
-                                <option value="Projeto 3"/>
-                                <option value="Projeto 4"/>
-                            </datalist>
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                        <option value="Projeto 4" />
+                    </datalist>
 
-                        <label htmlFor="minutesAmount">durante</label>
-                        <MinutesAmountInput
-                            type="number"
-                            id="minutesAmount"
-                            placeholder="00" 
-                            step={5}
-                            min={5}
-                            max={60}/>
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmount"
+                        placeholder="00"
+                        min={5}
+                        max={60}
+                        {...register('minutesAmount', { valueAsNumber: true })}
+                    />
 
-                        <span>minutos.</span>
+                    <span>minutos.</span>
                 </FormContainer>
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
-                <StartCountdownButton disabled type="submit">
+
+                <StartCountdownButton disabled={isSubmitDisabled} type="submit">
                     <Play size={24} />
                     Começar
                 </StartCountdownButton>
